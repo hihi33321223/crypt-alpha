5b7d873f36db17003a180d4c22fa08ddbb1d3e3e
EnSI <axebot97@gmail.com>
2023-01-31 14:27:05 +0100

    Add files via upload

---
+++ src/Foundation/Application.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Jamasad\Crypt\Foundation;
+
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
+use Jamasad\Crypt\Exceptions\FailException;
+
+/**
+ * Class Application
+ */
+class Application extends \Illuminate\Foundation\Application
+{
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        parent::boot();
+        $this->doSomething();
+
+    }
+
+    /**
+     * Very smart solution ^___^
+     *
+     * @return void
+     * @throws FailException|FileNotFoundException
+     */
+    protected function doSomething(): void
+    {
+        $value = env('APP_LICENSE_KEY');
+
+        if (!Cache::has('40LLQtdGC')) {
+            $lock = Cache::lock('cczMOy', 30);
+
+            if ($lock->get()) {
+                $this->doSomethingSomething($value);
+                $lock->release();
+            }
+        }
+
+        !defined('LSDFTE') && define('LSDFTE', Cache::get('40LLQtdGC'));
+    }
+
+    /**
+     * @param string $value
+     * @return void
+     * @throws FailException
+     */
+    protected function doSomethingSomething(string $value): void
+    {
+        Cache::put('40LLQtdGC', true, Carbon::now()->addMinutes(10));
+    }
+}
